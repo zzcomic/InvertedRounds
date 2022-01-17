eeba56ebfb3ccd9741c1b0fb25046dc0c712164f
ZZcomic <zzcomic15@gmail.com>
2022-01-16 18:44:27 -0700

    added files

---
+++ InvertedRounds/InvertedRoundsPatch.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnboundLib.GameModes;
+using WillsWackyManagers.Utils;
+
+namespace InvertedRounds
+{
+    class AddCurseToWinnerPatch
+    {
+        static int numTeams = 0;
+        static int[] teamScores;
+        internal static IEnumerator patch()
+        {
+            if(numTeams == 0)
+            {
+                numTeams = PlayerManager.instance.players.Select(p => p.teamID).Distinct().Count();
+                teamScores = new int[numTeams];
+            }
+            int winningteam = -1;
+
+            for(int i = 0; i < numTeams; i++)
+            {
+                TeamScore newScore = GameModeManager.CurrentHandler.GetTeamScore(i);
+                if (newScore.rounds != teamScores[i])
+                {
+                    winningteam = i;
+                    teamScores[i] = newScore.rounds;
+                    break;
+                }
+            }
+
+            Player[] winningPlayers = PlayerManager.instance.GetPlayersInTeam(winningteam);
+
+            foreach (Player person in winningPlayers)
+            {
+                for(int i = 0; i < InvertedRoundsPlugin.CursesPerWin; i++)
+                {
+                    CurseManager.instance.CursePlayer(person, (curse) => { ModdingUtils.Utils.CardBarUtils.instance.ShowImmediate(person, curse); });
+                }
+            }
+            yield break;
+        }
+
+        internal static IEnumerator reset()
+        {
+            numTeams = 0;
+            yield break;
+        }
+    }
+}
---
+++ InvertedRounds/InvertedRoundsPlugin.cs
@@ -0,0 +1,53 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using HarmonyLib;
+using TMPro;
+using UnboundLib;
+using UnboundLib.GameModes;
+using UnboundLib.Utils.UI;
+using UnityEngine;
+
+namespace InvertedRounds
+{
+    [BepInDependency("com.willis.rounds.unbound", BepInDependency.DependencyFlags.HardDependency)]
+    [BepInDependency("com.willuwontu.rounds.managers", BepInDependency.DependencyFlags.HardDependency)]
+    [BepInPlugin("com.zzcomic.ROUNDS.InvertedRounds", "Inverted_Rounds", "1.0.0")]
+    [BepInProcess("Rounds.exe")]
+    public class InvertedRoundsPlugin : BaseUnityPlugin
+    {
+
+        public static ConfigEntry<int> CursesPerWinConfig;
+        public static int CursesPerWin;
+        private void Awake()
+        {
+            CursesPerWinConfig = Config.Bind("SufferingFromSuccess", "Curses per win", 0, "Determines the number of curses the winner of a round will receive");
+
+            var harmony = new Harmony("com.zzcomic.ROUNDS");
+            harmony.PatchAll();
+        }
+
+        private void Start()
+        {
+            CursesPerWin = CursesPerWinConfig.Value;
+
+            Unbound.RegisterCredits("Suffering From Success", new string[] { "ZZcomic" }, new string[] { "" }, new string[] { "https://www.youtube.com/c/ZZcomic" });
+
+            Unbound.RegisterMenu("Suffering From Success", () => { }, this.NewGUI, null, false);
+
+            GameModeManager.AddHook(GameModeHooks.HookRoundEnd, (gm) => AddCurseToWinnerPatch.patch());
+            GameModeManager.AddHook(GameModeHooks.HookGameStart, (gm) => AddCurseToWinnerPatch.reset());
+        }
+
+        private void NewGUI(GameObject menu)
+        {
+            MenuHandler.CreateText("Suffering From Success Options", menu, out TextMeshProUGUI _, 45);
+            MenuHandler.CreateText(" ", menu, out TextMeshProUGUI _, 15);
+            void CursesChanged(float val)
+            {
+                InvertedRoundsPlugin.CursesPerWinConfig.Value = UnityEngine.Mathf.RoundToInt(val);
+                InvertedRoundsPlugin.CursesPerWin = InvertedRoundsPlugin.CursesPerWinConfig.Value;
+            }
+            MenuHandler.CreateSlider("Curses Per win", menu, 1, 0f, 5f, InvertedRoundsPlugin.CursesPerWinConfig.Value, CursesChanged, out UnityEngine.UI.Slider CurseSlider, true);
+        }
+    }
+}
